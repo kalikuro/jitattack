6f78abb98ef58f74840c1066b41bb4c8c7cec30d
Emmanuel Jerez <ejerez032776@gmail.com>
2022-11-28 15:46:02 -0800

    player moves

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,8 +5,8 @@
 
 int main(void){
 
-	int screenWidth = 800;
-	int screenHeight = 600;
+	int screenWidth = 1280;
+	int screenHeight = 720;
 
 	sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "Jit Attack", sf::Style::Close | sf::Style::Titlebar);
 	sf::Event event;
@@ -22,15 +22,30 @@ int main(void){
 	playerTexture.setSmooth(true);
 	playerTexture.setRepeated(false);
 	playerSprite.setTexture(playerTexture);
-	playerSprite.setPosition(sf::Vector2f(screenWidth / 2, screenHeight / 2));
+	playerSprite.setScale(sf::Vector2f(float(screenWidth) / 5000, float(screenHeight) / 2500));
+	playerSprite.setPosition(sf::Vector2f((screenWidth / 2 ) - ((playerTexture.getSize().x * playerSprite.getScale().x) / 2), (screenHeight / 2) - ((playerTexture.getSize().y * playerSprite.getScale().y) / 2)));
 
-	while (window.isOpen())
-	{
-		while (window.pollEvent(event))
-		{
+	while (window.isOpen()){
+
+		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		// move player
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+			playerSprite.move(0, -.25);
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+			playerSprite.move(-.25, 0);
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+			playerSprite.move(0, .25);
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+			playerSprite.move(.25, 0);
+		}
+
 
 		window.clear();
 
