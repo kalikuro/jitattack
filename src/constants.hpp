66d2e0b640c14820a1f88529e6ef244d086e5197
Emmanuel Jerez <ejerez032776@gmail.com>
2022-11-29 14:46:05 -0800

    adding game constants

---
+++ src/constants.hpp
@@ -0,0 +1,21 @@
+// game constants for game building
+// these will stay relative and constant throughout the game
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include "Utility/Types.hpp"
+
+// game constants
+#define title "Jit Attack";
+#define appFPS 60;
+#define appWidth 1280;
+#define appHeight 720;
+
+// math constants
+#define PI 3.1415f;
+#define PI2 6.2830f;
+#define degToRad 0.0174533f;
+#define radToDeg 57.2958f;
+
+#endif
