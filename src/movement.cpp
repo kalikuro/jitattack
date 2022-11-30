b02e49837b0f0e9932ee8ad798ba507525c2f58d
Emmanuel Jerez <ejerez032776@gmail.com>
2022-11-29 16:42:26 -0800

    jerry can now shoot

--- src/movement.cpp
+++ src/movement.cpp
@@ -180,10 +180,11 @@
 
 #include "movement.hpp"
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-const float Movement::maxSpeed = 0.5f;
-const float Movement::acl = 1.0f;
-const float Movement::rotationSpeed = 0.5f;
+const float Movement::maxSpeed = 0.1f;
+const float Movement::acl = 0.1f;
+const float Movement::rotationSpeed = 0.1f;
 
 
 Movement::Movement(){
@@ -208,38 +209,89 @@ void Movement::moveSprite(sf::Sprite &sprite){
 	// 	sprite.move(0, 1);
 	// }
 
-	if(mX == 1){
-		velocity.x += acl;
+	// if(mX == 1){
+	// 	velocity.x += acl;
+	// }
+	// if(mX == -1){
+	// 	velocity.x -= acl;
+	// }
+	// if(mY == 1){
+	// 	velocity.y += acl;
+	// }
+	// if(mY == -1){
+	// 	velocity.y -= acl;
+	// }
+
+	// if(velocity.x > maxSpeed){
+	// 	velocity.x = maxSpeed;
+	// }
+	// if(velocity.x < -maxSpeed){
+	// 	velocity.x = -maxSpeed;
+	// }
+	// if(velocity.y > maxSpeed){
+	// 	velocity.y = maxSpeed;
+	// }
+	// if(velocity.y < -maxSpeed){
+	// 	velocity.y = -maxSpeed;
+	// }
+
+	// if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+	// 	sprite.rotate(-rotationSpeed);
+	// }
+	// if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+	// 	sprite.rotate(rotationSpeed);
+	// }
+
+	mX = 0;
+	mY = 0;
+
+	// if(mX != 0){
+	// 	sprite.rotate(mX * rotationSpeed);
+	// }
+
+	// if(mY != 0){
+
+	// }
+
+	// if(velocity.x > maxSpeed){
+	// 	velocity.x = maxSpeed;
+	// }
+	// if(velocity.x < -maxSpeed){
+	// 	velocity.x = -maxSpeed;
+	// }
+	// if(velocity.y > maxSpeed){
+	// 	velocity.y = maxSpeed;
+	// }
+	// if(velocity.y < -maxSpeed){
+	// 	velocity.y = -maxSpeed;
+	// }
+
+	// if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+
+	// }
+
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+		mX = -1;
 	}
-	if(mX == -1){
-		velocity.x -= acl;
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+		mX = 1;
 	}
-	if(mY == 1){
-		velocity.y += acl;
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+		mY = 1;
 	}
-	if(mY == -1){
-		velocity.y -= acl;
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+		mY = -1;
 	}
 
-	if(velocity.x > maxSpeed){
-		velocity.x = maxSpeed;
-	}
-	if(velocity.x < -maxSpeed){
-		velocity.x = -maxSpeed;
-	}
-	if(velocity.y > maxSpeed){
-		velocity.y = maxSpeed;
-	}
-	if(velocity.y < -maxSpeed){
-		velocity.y = -maxSpeed;
+	if(mX != 0){
+		sprite.rotate(mX * rotationSpeed);
 	}
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-		sprite.rotate(-rotationSpeed);
+	if(mY != 0){
+		velocity.x += mY * acl * cos(sprite.getRotation() * 3.14159265 / 180);
+		velocity.y += mY * acl * sin(sprite.getRotation() * 3.14159265 / 180);
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
-		sprite.rotate(rotationSpeed);
-	}
+	sprite.move(velocity);
 }
 
 void Movement::wrapSprite(sf::Sprite &sprite, sf::RenderWindow &window){
