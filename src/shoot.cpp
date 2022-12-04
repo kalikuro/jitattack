d4ad802dd82e71636f8061b67d0cc364d637163a
Emmanuel Jerez <emmanueljerez@Emmanuels-Air.lan>
2022-12-03 17:50:57 -0800

    changes

--- src/shoot.cpp
+++ src/shoot.cpp
@@ -19,13 +19,10 @@ Shoot::~Shoot(){
 
 void Shoot::onShoot(sf::Sprite &sprite, sf::RenderWindow &window){
 	playerPos = sprite.getPosition();
-	aimDirection = shootVel/sqrt(pow(shootVel.x, 2) + pow(shootVel.y, 2));
+	aimDirection = shootVel/(sqrt(pow(shootVel.x, 2) + pow(shootVel.y, 2)));
 
 	if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-		bullets.push_back(Shoot());
-		bullets.back().shootVel = aimDirection;
-		bullets.back().shootVel.x *= speed;
-		bullets.back().shootVel.y *= speed;
+
 	}
 
 	for(int i = 0; i < bullets.size(); i++){
