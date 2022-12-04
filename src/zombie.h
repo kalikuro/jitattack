4259a6096b4e90c643ee6d417f99b79ac3fb47ac
Emmanuel Jerez <ejerez032776@gmail.com>
2022-12-03 17:29:50 -0800

    this shit deosnt want to compile

--- src/zombie.h
+++ src/zombie.h
@@ -1,26 +1,26 @@
-#ifndef Zombie_H
-#define Zombie_H
+// #ifndef Zombie_H
+// #define Zombie_H
 
-#include <iostream>
-#include <string>
-#include <SFML/Graphics.hpp>
+// #include <iostream>
+// #include <string>
+// #include <SFML/Graphics.hpp>
 
-class zombie{
-	public:
-		zombie(){};
-		zombie(const int, float);
-		sf:: Sprite & getSprite();
-		void selectLocation(float, float);
-		void kill();
-		bool isAlive();
-		float getSpeed() const;
-		void draw(sf::RenderWindow & win);
+// class zombie{
+// 	public:
+// 		zombie(){};
+// 		zombie(const int, float);
+// 		sf:: Sprite & getSprite();
+// 		void selectLocation(float, float);
+// 		void kill();
+// 		bool isAlive();
+// 		float getSpeed() const;
+// 		void draw(sf::RenderWindow & win);
 
-	private:
-		sf::Sprite zombieSprite;
-		sf::Texture zombieTexture;
-		bool alive;
-		float speed;
-};
+// 	private:
+// 		sf::Sprite zombieSprite;
+// 		sf::Texture zombieTexture;
+// 		bool alive;
+// 		float speed;
+// };
 
-#endif
+// #endif
