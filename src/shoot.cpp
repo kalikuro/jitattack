9fdbe84e0f8910634e2dc238e710a9e7636c528c
Emmanuel Jerez <emmanueljerez@Emmanuels-Air.lan>
2022-12-03 17:45:06 -0800

    bruh

--- src/shoot.cpp
+++ src/shoot.cpp
@@ -5,6 +5,7 @@
 #include "shoot.hpp"
 #include "movement.hpp"
 
+Shoot b;
 std::vector<Shoot> bullets;
 const float circleRadius = 5.f;
 const float speed = 0.1f;
@@ -17,18 +18,18 @@ Shoot::~Shoot(){
 }
 
 void Shoot::onShoot(sf::Sprite &sprite, sf::RenderWindow &window){
-	playerPos = Vector2f(sprite.getPosition().x, sprite.getPosition().y);
+	playerPos = sprite.getPosition();
 	aimDirection = shootVel/sqrt(pow(shootVel.x, 2) + pow(shootVel.y, 2));
 
 	if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-		bullet.shape.setPositon(playerPos);
-		bullet.shootVel = aimDirection * speed;
-		bullets.push_back(bullet);
+		bullets.push_back(Shoot());
+		bullets.back().shootVel = aimDirection;
+		bullets.back().shootVel.x *= speed;
+		bullets.back().shootVel.y *= speed;
 	}
 
 	for(int i = 0; i < bullets.size(); i++){
-		bullets[i].shape.move(bullets[i].shootVel);
-		window.draw(bullets[i].shape);
+
 	}
 
 
